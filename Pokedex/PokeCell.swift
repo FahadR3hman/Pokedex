60fcf48ad10f5c2181c0691459d7fd09a9b13b0e
Fahad Rehman <Fahad@macs-MacBook-Air.local>
2016-11-20 00:25:07 +0500

    Using Collection View

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,34 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Fahad Rehman on 11/19/16.
+//  Copyright © 2016 Codecture. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImage: UIImageView!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon : Pokemon!
+    
+    required init?(coder aDecoder:NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 6.0
+    }
+    
+    func configurecell (pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = pokemon.name.capitalized
+        thumbImage.image = UIImage(named: "\(pokemon.pokedexId)")
+        
+    }
+    
+}
---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Fahad Rehman on 11/19/16.
+//  Copyright © 2016 Codecture. All rights reserved.
+//
+
+import Foundation
+class Pokemon {
+    
+    private var _name : String!
+    private var _pokedexId : Int!
+    
+    var name :String {
+        return _name
+    }
+    
+    var pokedexId : Int {
+        return _pokedexId
+    }
+    
+    init(name: String , pokedexId: Int) {
+    
+        _name = name
+        _pokedexId = pokedexId
+        
+    }
+    
+    
+}
